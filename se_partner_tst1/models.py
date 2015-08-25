7e023c6b33432e13840b79e630f79de33841f058
boss <bossboss@front.ru>
2015-08-25 17:25:06 +0500

    1 перенос данных

---
+++ se_partner_tst1/models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+
+# class se_partner_tst1(models.Model):
+#     _name = 'se_partner_tst1.se_partner_tst1'
+
+#     name = fields.Char()
