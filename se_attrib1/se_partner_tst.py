b9f6e84faf2a87f149f07fb9b530f465ded89d01
boss <bossboss@front.ru>
2015-10-16 14:23:59 +0500

    добавлен пункт меню

--- se_attrib1/se_partner_tst.py
+++ se_attrib1/se_partner_tst.py
@@ -2,42 +2,42 @@
 from openerp import models, fields, api
 class se_category(models.Model):
     _inherit = 'product.category'
-    ef_grpt = fields.Integer('Группа продукта')
-    ef_kat_vidt = fields.Integer('Категория продукта')
-    ef_kat_vidt_kat_grpt_kod = fields.Integer('Родительская категория продукта')
+    ef_grpt = fields.Integer('Группа продукта', select=True)
+    ef_kat_vidt = fields.Integer('Категория продукта', select=True)
+    ef_kat_vidt_kat_grpt_kod = fields.Integer('Родительская категория продукта', select=True)
 class se_template(models.Model):
     _inherit = 'product.template'
-    ef_id = fields.Integer('Код продукта из ЛО')
+    ef_id = fields.Integer('Код продукта из ЛО', select=True)
 class se_product(models.Model):
     _inherit = 'product.product'
-    ef_id = fields.Integer('Код продукта из ЛО')
+    ef_id = fields.Integer('Код продукта из ЛО', select=True)
 class se_partner(models.Model):
     _inherit = 'res.partner'
-    ef_id = fields.Integer('Код контрагента из ЛО')
-    ef_id_adr = fields.Integer('Код адреса из KLN_ADK')
-    ef_id_sot = fields.Integer('Код сотрудника из ЛО')
+    ef_id = fields.Integer('Код контрагента из ЛО', select=True)
+    ef_id_adr = fields.Integer('Код адреса из KLN_ADK', select=True)
+    ef_id_sot = fields.Integer('Код сотрудника из ЛО', select=True)
 class product_pricelist(models.Model):
     _inherit = 'product.pricelist'
-    ef_is_comp = fields.Boolean('ЭтоАкция')
-    ef_PartnerId = fields.Many2one('res.partner', ondelete='set null', string='Контрагент',)
+    ef_is_comp = fields.Boolean('ЭтоАкция', select=True)
+    ef_PartnerId = fields.Many2one('res.partner', ondelete='set null', string='Контрагент', select=True)
 class hr_department(models.Model):
     _inherit = 'hr.department'
-    ef_id = fields.Integer('Код отдела из ЛО')
+    ef_id = fields.Integer('Код отдела из ЛО', select=True)
 class hr_employee(models.Model):
     _inherit = 'hr.employee'
-    ef_id = fields.Integer('Код сотрудника из ЛО')
+    ef_id = fields.Integer('Код сотрудника из ЛО', select=True)
 class res_users(models.Model):
     _inherit = 'res.users'
-    ef_id_sot = fields.Integer('Код сотрудника из ЛО')
+    ef_id_sot = fields.Integer('Код сотрудника из ЛО', select=True)
 class resource_resource(models.Model):
     _inherit = 'resource.resource'
-    ef_id_sot = fields.Integer('Код сотрудника из ЛО')
+    ef_id_sot = fields.Integer('Код сотрудника из ЛО', select=True)
 class res_company(models.Model):
     _inherit = 'res.company'
-    ef_id = fields.Integer('Код клиента из KAT_KLN ЛО')
+    ef_id = fields.Integer('Код клиента из KAT_KLN ЛО', select=1)
 class account_bank_statement(models.Model):
     _inherit = 'account.bank.statement'
-    rch_b = fields.Char('Расчетный счет из ЛО',size=20)
+    rch_b = fields.Char('Расчетный счет из ЛО',size=20, select=True)
 class account_invoice(models.Model):
     _inherit = 'account.invoice'
  #   ef_id = fields.Integer('Код строки из LIST_SKL ЛО')
@@ -49,5 +49,5 @@ class account_invoice_line(models.Model):
   #  ставим вручную bigint
 class stock_warehouse(models.Model):
     _inherit = 'stock.warehouse'
-    ef_id = fields.Integer('Код строки из KAT_SKL ЛО')
+    ef_id = fields.Integer('Код строки из KAT_SKL ЛО', select=True)
     
