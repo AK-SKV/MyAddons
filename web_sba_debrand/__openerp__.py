1f8a08768265a6a122ba20014335a40d9998c493
VbossEkat <bossboss@front.ru>
2015-12-22 19:58:44 +0500

    дебрэндинг в рабочем виде

---
+++ web_sba_debrand/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Backend SBA debranding",
+    'version': '1.0.0',
+    'author': 'SBA Glukhov',
+    'license': 'GPL-3',
+    'category': 'Debranding',
+    'website': 'https://sba-group.ru',
+    'depends': ['web', 'share', 'disable_openerp_online', 'mail_delete_sent_by_footer'],
+    'data': [
+        'views.xml',
+        ],
+    'qweb': [
+        'templ.xml',
+        ],
+    'auto_install': False,
+    'installable': True
+}
