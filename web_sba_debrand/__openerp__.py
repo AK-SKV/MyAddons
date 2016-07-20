22d34be2818934221aecd572c324acdcf0259e46
VbossEkat <bossboss@front.ru>
2016-07-20 11:14:09 +0500

    незначительные поправки

---
+++ web_sba_debrand/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Backend SBA debranding",
+    'version': '1.0.0',
+    'author': 'SBA Glukhov',
+    'license': 'GPL-3',
+    'category': 'Debranding',
+    'website': 'https://sba-group.ru',
+    'depends': ['web', 'share', 'mail_delete_sent_by_footer'],
+    'data': [
+        'views.xml',
+        'js.xml',
+        ],
+    'qweb': [
+        'templ.xml',
+        ],
+    'auto_install': False,
+    'installable': True
+}
